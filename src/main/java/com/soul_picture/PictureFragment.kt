c61ddc6cd26bd8a4abd5141bd22ec49db141a905
chanzhilin <1181785848@qq.com>
2019-06-20 14:50:19 +0800

    module 的配置

---
+++ src/main/java/com/soul_picture/PictureFragment.kt
@@ -0,0 +1,59 @@
+package com.soul_picture
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.kotlin_baselib.base.BaseFragment
+import com.kotlin_baselib.base.EmptyModelImpl
+import com.kotlin_baselib.base.EmptyPresenterImpl
+import com.kotlin_baselib.base.EmptyView
+import kotlinx.android.synthetic.main.fragment_picture.*
+
+
+private const val ARG_PARAM1 = "param1"
+
+/**
+ *  Created by CHEN on 2019/6/20
+ *  Email:1181785848@qq.com
+ *  Package:com.soul_picture
+ *  Introduce:
+ **/
+class PictureFragment : BaseFragment<EmptyView, EmptyModelImpl, EmptyPresenterImpl>(), EmptyView {
+
+    override fun createPresenter(): EmptyPresenterImpl {
+        return EmptyPresenterImpl(this)
+    }
+
+    private var param1: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+        }
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String) =
+            PictureFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                }
+            }
+    }
+
+    override fun getResId(): Int {
+        return R.layout.fragment_picture
+    }
+
+    override fun initData() {
+        fragment_text.setText(param1)
+    }
+
+    override fun initListener() {
+    }
+
+    override fun lazyLoad() {
+    }
+}
