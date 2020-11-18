3c9fdb06cbb67998db6637bddcd36dc4dbf9d4e5
chanzhilin <1181785848@qq.com>
2020-11-18 18:03:00 +0800

    some code change

--- src/main/java/com/soul_picture/main/PictureDetailActivity.kt
+++ src/main/java/com/soul_picture/main/PictureDetailActivity.kt
@@ -3,13 +3,11 @@ package com.soul_picture.main
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.graphics.Color
-import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
-import android.widget.FrameLayout
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
+import android.view.animation.DecelerateInterpolator
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.flask.colorpicker.ColorPickerView
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder
@@ -17,10 +15,11 @@ import com.kotlin_baselib.base.BaseViewModelActivity
 import com.kotlin_baselib.base.EmptyViewModel
 import com.kotlin_baselib.glide.GlideUtil
 import com.kotlin_baselib.recyclerview.setSingleItemUp
-import com.kotlin_baselib.utils.ScreenUtils
+import com.kotlin_baselib.utils.SdCardUtil
 import com.kotlin_baselib.utils.SnackBarUtil
 import com.soul_picture.R
 import kotlinx.android.synthetic.main.activity_picture_detail.*
+import kotlinx.android.synthetic.main.dialog_brush_bottom.*
 import kotlinx.android.synthetic.main.layout_item_picture_detail_edit_bottom.view.*
 
 
@@ -49,76 +48,99 @@ class PictureDetailActivity : BaseViewModelActivity<EmptyViewModel>() {
 
 
     override fun initData() {
-        if (ScreenUtils.instance.checkDeviceHasNavigationBar()) {   //有虚拟导航栏
+        /*    if (ScreenUtils.instance.checkDeviceHasNavigationBar()) {   //有虚拟导航栏
 
-            val params = FrameLayout.LayoutParams(picture_detial_fl_bottom.layoutParams)
-            params.gravity = Gravity.BOTTOM
-            params.bottomMargin = ScreenUtils.instance.getNavigationBarHeight()
-            picture_detial_fl_bottom.layoutParams = params
-        }
+                val params = ConstraintLayout.LayoutParams(picture_detial_fl_bottom.layoutParams)
+                params.bottomMargin = ScreenUtils.instance.getNavigationBarHeight()
+                picture_detial_fl_bottom.layoutParams = params
+            }*/
         path = intent.getStringExtra("keyImage")
 
-        GlideUtil.instance.loadImage(this, path, picture_detial_iv)
+        GlideUtil.instance.loadImage(this, path, picture_detail_iv)
+
+
+        brush.apply {
+            dialog_bursh_seekbar?.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    dialog_bursh_text?.text = "画笔粗细值为：${progress}"
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            })
+
+        }
+
+        brush?.translationY = 800f
+        brush?.findViewById<TextView>(R.id.dialog_toolbar_close)
+            ?.setOnClickListener {
+                isShowDialog = false
+                brush?.animate()?.apply {
+                    duration = 200
+                    interpolator = DecelerateInterpolator()
+                    translationY(brush.height.toFloat())
+                }?.start()
+                brush?.postDelayed({
+                    brush?.visibility = View.GONE
+                }, 200)
+            }
+        brush?.findViewById<TextView>(R.id.dialog_toolbar_done)
+            ?.setOnClickListener {
+                isShowDialog = false
+                brush?.animate()?.apply {
+                    duration = 200
+                    interpolator = DecelerateInterpolator()
+                    translationY(brush.height.toFloat())
+                }?.start()
+                brush?.postDelayed({
+                    brush?.visibility = View.GONE
+                }, 200)
+
+            }
 
 
         data = ArrayList<String>()
+        data.add("发送")
         data.add("编辑")
+        data.add("收藏")
+        data.add("删除")
+        data.add("更多")
         picture_detial_rv.setSingleItemUp(
             data,
             R.layout.layout_item_picture_detail_edit_bottom,
 
-            { _,holder, item ->
+            { _, holder, item ->
                 holder.itemView.item_picture_detail_bottom_tv.text = item
             },
             LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false),
-            {  position, it ->
-                if (it.equals("编辑")) {
-                    val objectAnimatorX: ObjectAnimator =
-                        ObjectAnimator.ofFloat(picture_detial_iv, "scaleX", 1f, 0.7f)
-                    val objectAnimatorY: ObjectAnimator =
-                        ObjectAnimator.ofFloat(picture_detial_iv, "scaleY", 1f, 0.7f)
-                    val objectAnimatorTranlateY =
-                        ObjectAnimator.ofFloat(picture_detial_iv, "translationY", 0f, -100f)
-                    val animatorSet = AnimatorSet()
-                    animatorSet.playTogether(
-                        objectAnimatorX,
-                        objectAnimatorY,
-                        objectAnimatorTranlateY
-                    )
-                    animatorSet.start()
-
-                    picture_detial_tv_switch_color.visibility = View.VISIBLE    //显示切换颜色的按钮
-                    data.clear()
-                    data.add("涂鸦")
-                    data.add("添加文本")
-                    data.add("马赛克")
-                    data.add("上一步")
-                    data.add("下一步")
-                    data.add("完成")
-                    picture_detial_rv.adapter!!.notifyDataSetChanged()
-                    isReset = false
-                }
-                if (it.equals("涂鸦")) {
-
-                    val layout =
-                        LayoutInflater.from(mContext).inflate(R.layout.dialog_brush_bottom, null)
-                    layout.setBackgroundColor(ContextCompat.getColor(mContext, R.color.white))
-                    picture_detial_fl_bottom.addView(layout)
-                    isShowDialog = true
-
-                    layout.findViewById<ImageView>(R.id.dialog_toolbar_close).setOnClickListener {
-                        isShowDialog = false
-                        picture_detial_fl_bottom.removeView(layout)
+            { position, it ->
+                when (it) {
+                    "编辑" -> {
+                        showEditBottomView()
                     }
-                    layout.findViewById<ImageView>(R.id.dialog_toolbar_done).setOnClickListener {
-                        isShowDialog = false
-                        picture_detial_fl_bottom.removeView(layout)
+                    "删除" ->{
+                        SdCardUtil.deleteFile(path){
+                            SnackBarUtil.shortSnackBar(
+                                picture_detail_iv,
+                                if (it) "删除图片成功" else "删除图片失败！",
+                                SnackBarUtil.CONFIRM
+                            ).show()
+                            if (it){
+                                picture_detial_rv?.postDelayed({
+                                    finishAfterTransition()
+                                },500)
+                            }
+                        }
                     }
-
-
-                }
-                if (it.equals("完成")) {
-                    reset()
                 }
             }
         )
@@ -126,30 +148,31 @@ class PictureDetailActivity : BaseViewModelActivity<EmptyViewModel>() {
     }
 
     private fun reset() {
-
-        val objectAnimatorX: ObjectAnimator =
-            ObjectAnimator.ofFloat(picture_detial_iv, "scaleX", 0.7f, 1.0f)
-        val objectAnimatorY: ObjectAnimator =
-            ObjectAnimator.ofFloat(picture_detial_iv, "scaleY", 0.7f, 1.0f)
-        val objectAnimatorTranlateY =
-            ObjectAnimator.ofFloat(picture_detial_iv, "translationY", -100f, 0f)
+        val ivTranslateY =
+            ObjectAnimator.ofFloat(picture_detail_iv, "translationY", -100f, 0f)
+        val rvEditTranslateY =
+            ObjectAnimator.ofFloat(
+                picture_detial_rv,
+                "translationY",
+                100f,
+                0f
+            )
+        rvEditTranslateY.duration = 500
+        rvEditTranslateY?.interpolator = DecelerateInterpolator()
         val animatorSet = AnimatorSet()
         animatorSet.playTogether(
-            objectAnimatorX,
-            objectAnimatorY,
-            objectAnimatorTranlateY
+            ivTranslateY, rvEditTranslateY
         )
         animatorSet.start()
-        picture_detial_tv_switch_color.visibility = View.GONE       //隐藏切换颜色的按钮
-        data.clear()
-        data.add("编辑")
-        picture_detial_rv.adapter!!.notifyDataSetChanged()
+        picture_detial_tv_switch_color?.visibility = View.GONE       //隐藏切换颜色的按钮
+        picture_detial_rv?.visibility = View.VISIBLE
+        rv_edit?.visibility = View.GONE
         isReset = true
 
     }
 
     override fun initListener() {
-        picture_detial_tv_switch_color.setOnClickListener {
+        picture_detial_tv_switch_color?.setOnClickListener {
             ColorPickerDialogBuilder
                 .with(this)
                 .setTitle("选择颜色")
@@ -157,11 +180,11 @@ class PictureDetailActivity : BaseViewModelActivity<EmptyViewModel>() {
                 .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
                 .density(12)
                 .setPositiveButton(
-                    "ok"
+                    "确定"
                 )
                 { dialog, selectedColor, allColors ->
                     SnackBarUtil.shortSnackBar(
-                        picture_detial_iv,
+                        picture_detail_iv,
                         "${selectedColor}",
                         SnackBarUtil.CONFIRM
                     ).show()
@@ -173,9 +196,64 @@ class PictureDetailActivity : BaseViewModelActivity<EmptyViewModel>() {
         }
     }
 
+    private fun showEditBottomView() {
+        rv_edit?.setSingleItemUp(
+            mutableListOf("涂鸦", "添加文本", "上一步", "下一步", "完成"),
+            R.layout.layout_item_picture_detail_edit_bottom,
+            { _, holder, item ->
+                holder.itemView.item_picture_detail_bottom_tv.text = item
+            },
+            LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false),
+            { position, data ->
+                when (data) {
+                    "涂鸦" -> {
+                        brush?.alpha = 0f
+                        brush?.visibility = View.VISIBLE
+                        brush?.animate()
+                            ?.setDuration(200)
+                            ?.alpha(1f)
+                            ?.setInterpolator(DecelerateInterpolator())
+                            ?.translationY(0f)
+                            ?.setStartDelay(100)
+                            ?.start()
+                        isShowDialog = true
+                    }
+                    "完成" -> {
+                        reset()
+                    }
+                }
+            }
+        )
+
+        rv_edit?.visibility = View.VISIBLE
+        picture_detial_tv_switch_color?.visibility = View.VISIBLE    //显示切换颜色的按钮
+        picture_detial_rv?.visibility = View.GONE
+        isReset = false
+
+        val ivTranslateY =
+            ObjectAnimator.ofFloat(picture_detail_iv, "translationY", 0f, -100f)
+        ivTranslateY.duration = 500
+        ivTranslateY?.interpolator = DecelerateInterpolator()
+        val rvEditTranslateY =
+            ObjectAnimator.ofFloat(
+                rv_edit,
+                "translationY",
+                100f,
+                0f
+            )
+        rvEditTranslateY.duration = 500
+        rvEditTranslateY?.interpolator = DecelerateInterpolator()
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(
+            ivTranslateY, rvEditTranslateY
+        )
+        animatorSet.start()
+    }
+
+
     override fun onBackPressed() {
         if (isShowDialog) {
-            picture_detial_fl_bottom.removeViewAt(picture_detial_fl_bottom.childCount - 1)
+            brush?.visibility = View.GONE
             isShowDialog = false
             return
         }
